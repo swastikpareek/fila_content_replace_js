a1b9b15b5948a65572241cc3f540cb869fce60bb
Swastik Pareek <swastik@qed42.com>
2015-09-05 16:55:41 +0530

    Js tweak for reading drupal.settings var properly

--- fila_content_replace_script.js
+++ fila_content_replace_script.js
@@ -1,46 +1,3 @@
-// var CONTENTREPLACE = CONTENTREPLACE || (function() {
-//   var _args = {}; // private
-
-//   return {
-//     init: function(Args) {
-//       _args = Args;
-//       // some other initialising
-//     },
-//     executeAjaxCall: function() {
-//       $(document).ready(function() {
-//         function createXMLHttpRequest() {
-//           try {
-//             return new XMLHttpRequest();
-//           } catch (e) {}
-//           try {
-//             return new ActiveXObject("Msxml2.XMLHTTP");
-//           } catch (e) {}
-//           alert("XMLHttpRequest not supported");
-//           return null;
-//         }
-//         var xhReq = createXMLHttpRequest();
-//         xhReq.open("GET", _args[0], false);
-//         xhReq.send(null);
-//         var serverResponse = xhReq.responseText;
-//         var data = serverResponse.replace(/\n/g, "");
-//         $obj = $(data);
-//         $obj.each(function(key, item) {
-//           if (item.className === 'mainContentRegion') {
-//             $(_args[1]).html('');
-//             $(_args[1]).append(item);
-//           }
-//           if (item.nodeName === 'SCRIPT') {
-//             $('body').append(item);
-//           }
-//           if (item.nodeName === 'STYLE') {
-//             $('head').append(item);
-//           }
-//         });
-//       });
-//     }
-//   };
-// }());
-
 var CONTENTREPLACE = CONTENTREPLACE || (function() {
   var _args = {}; // private
 
@@ -54,13 +11,12 @@ var CONTENTREPLACE = CONTENTREPLACE || (function() {
         var allContentLoaded = false;
         $(_args[1]).empty();
         $(_args[1]).addClass('CORScontentIsLoading');
-        $(_args[1]).append('<div class="CORScontentLoaderDiv">Loading Your Content Please wait...</div>');
+        $(_args[1]).find('mainContentRegion').append('<div class="CORScontentLoaderDiv">Loading Your Content Please wait...</div>');
 
         function Exception(type, message) {
           this.message = message;
           this.name = type;
         }
-        // console.log($('#body').text(''));
         $.ajax({
           url: _args[0],
           type: "GET",
@@ -89,7 +45,7 @@ var CONTENTREPLACE = CONTENTREPLACE || (function() {
                   throw new Exception('contentUndefined', 'Drupal is not Defined');
                 } else {
                   if (allContentLoaded) {
-                    Drupal.behaviors.homePageSettings.attach();
+                    Drupal.behaviors.homePageSettings.attach(true);
                     clearInterval(checkContent);
                   }
                 }
