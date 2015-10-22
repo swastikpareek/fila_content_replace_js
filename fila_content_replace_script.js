c82f4104d4be8ac0cc1d26da590fc1689f5a3584
Swastik Pareek <swastik@qed42.com>
2015-10-22 15:55:01 +0530

    Called code thru get script func

--- fila_content_replace_script.js
+++ fila_content_replace_script.js
@@ -51,6 +51,7 @@ var CONTENTREPLACE = CONTENTREPLACE || (function() {
             //First of adding Drupal.settings to an array
             scriptsArray.forEach(function(d, i) {
               if (d.getAttribute('src') === null) {
+                jQuery('body').append(d);
                 if (d.innerHTML.indexOf('Drupal.settings') > -1) {
                   var drupalSettingsJSON = d.innerHTML.substring(d.innerHTML.indexOf('jQuery.extend(Drupal.settings,'), d.innerHTML.indexOf('//--><!]]>'));
                   swastik = drupalSettingsJSON;
@@ -58,32 +59,49 @@ var CONTENTREPLACE = CONTENTREPLACE || (function() {
               }
             });
             //After that adding all scripts other than custom.js and contentreplace.js
-            scriptsArray.forEach(function(d, i) {
-              if (d.getAttribute('src') !== null && (d.getAttribute('src').indexOf('custom.js') === -1 && d.getAttribute('src').indexOf('contentReplaceScriptAddon.js') === -1)) {
+            // scriptsArray.forEach(function(d, i) {
+            //   if (d.getAttribute('src') !== null && (d.getAttribute('src').indexOf('custom.js') === -1 && d.getAttribute('src').indexOf('contentReplaceScriptAddon.js') === -1)) {
 
-                // console.log('check' + d.getAttribute('src').indexOf('jquery.min.js'));
-                d.setAttribute('defer', true);
-                jQuery('body').append(d);
-              }
-            });
-            //Adding custom.js file
-            scriptsArray.forEach(function(d, i) {
-              if (d.getAttribute('src') !== null) {
-                if (d.getAttribute('src').indexOf('custom.js') > -1) {
-                  d.setAttribute('defer', true);
-                  jQuery('body').append(d);
+            //     // console.log('check' + d.getAttribute('src').indexOf('jquery.min.js'));
+            //     d.setAttribute('async', true);
+            //     jQuery('body').append(d);
+            //   }
+            // });
+            // //Adding custom.js file
+            // scriptsArray.forEach(function(d, i) {
+            //   if (d.getAttribute('src') !== null) {
+            //     if (d.getAttribute('src').indexOf('custom.js') > -1) {
+            //       d.setAttribute('defer', true);
+            //       jQuery('body').append(d);
+            //     }
+            //   }
+            // });
+            // // ADding contentReplaceScriptAddon.js lastly to ensure delayed execution
+            // scriptsArray.forEach(function(d, i) {
+            //   if (d.getAttribute('src') !== null) {
+            //     if (d.getAttribute('src').indexOf('contentReplaceScriptAddon.js') > -1) {
+            //       d.setAttribute('defer', true);
+            //       jQuery('body').append(d);
+            //     }
+            //   }
+            // });
+            var url = 'http://localhost/fila-demo/na/';
+
+            function getScripts(i) {
+              if (scriptsArray[i] !== undefined) {
+                if (scriptsArray[i].getAttribute('src') !== null && scriptsArray[i].getAttribute('src').indexOf('http://') === -1) {
+                  var req_arr = url + scriptsArray[i].getAttribute('src');
+                  console.log(req_arr);
+                  jQuery.getScript(req_arr, function() {
+                    // console.log(scriptsArray[i].getAttribute('src'));
+                    getScripts(i + 1);
+                  });
+                } else {
+                  getScripts(i + 1);
                 }
               }
-            });
-            // ADding contentReplaceScriptAddon.js lastly to ensure delayed execution
-            scriptsArray.forEach(function(d, i) {
-              if (d.getAttribute('src') !== null) {
-                if (d.getAttribute('src').indexOf('contentReplaceScriptAddon.js') > -1) {
-                  d.setAttribute('defer', true);
-                  jQuery('body').append(d);
-                }
-              }
-            });
+            }
+            getScripts(0);
           },
           error: function(xhr, status) {
             console.log("Sorry, there was a problem! Status " + status);
