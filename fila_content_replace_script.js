0870620002c34a7b5ddddc506230e4f8da85be7e
Swastik Pareek <swastik@qed42.com>
2016-01-12 21:26:32 +0530

    Removed eval Drupal.attachbehaviour()

--- fila_content_replace_script.js
+++ fila_content_replace_script.js
@@ -73,9 +73,6 @@ var CONTENTREPLACE = CONTENTREPLACE || (function() {
                   getScripts(i + 1);
                 }
               }
-              else{
-                eval(Drupal.attachBehaviors());
-              }
             }
             getScripts(0);
           },
